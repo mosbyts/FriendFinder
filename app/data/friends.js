bf8ba8e1b2281bc8f610cf087fde4edafbf46b41
Tianna Mosby <mosbyts@vcu.edu>
2019-11-04 22:19:01 -0500

    Updated styling and logic.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friends = {
-  "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "name":"Bob Belcher",
+  "photo":"https://vignette.wikia.nocookie.net/bobs-burgers-fanon/images/5/51/Bob_with_a_burger.png/revision/latest/scale-to-width-down/298?cb=20170920181424",
   "scores":[
       5,
       1,
@@ -12,7 +12,67 @@ var friends = {
       5,
       4,
       1
-    ]
+    ],
+  
+  "name":"Tina Belcher",
+  "photo":"http://images6.fanpop.com/image/photos/38500000/Tina-Belcher-bobs-burgers-38510376-1080-1858.jpg",
+  "scores":[
+      1,
+      4,
+      5,
+      2,
+      1,
+      5,
+      4,
+      4,
+      1,
+      5
+    ],
+
+  "name":"Louise Belcher",
+  "photo":"https://www.thetvdb.com/banners/actors/239951.jpg",
+  "scores":[
+      2,
+      5,
+      4,
+      3,
+      2,
+      4,
+      5,
+      5,
+      2,
+      1
+    ],
+
+    "name":"Linda Belcher",
+    "photo":"https://vignette.wikia.nocookie.net/bobsburgerpedia/images/b/b7/Lindab.png/revision/latest/scale-to-width-down/200?cb=20130113071354",
+    "scores":[
+        4,
+        3,
+        4,
+        4,
+        3,
+        2,
+        1,
+        4,
+        3,
+        1
+      ],
+
+      "name":"Jimmy Pesto, Jr.",
+      "photo":"https://vignette.wikia.nocookie.net/bobsburgerpedia/images/7/70/Jimmy_pesto_junior.png/revision/latest?cb=20160215225017",
+      "scores":[
+        4,
+        3,
+        4,
+        4,
+        3,
+        2,
+        1,
+        4,
+        3,
+        1
+        ],
 };
 
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,17 +1,34 @@
-//Load data
+//Load dependecies/data
+var path = require("path");
 var friends = require("../data/friends");
 
 module.exports = function(app){
 //Display all possible friends in the JSON object friends
    app.get("/api/friends", function(req, res){
-      res.json(friends);
+      return res.json(friends);
    });
 
 //Handle incoming survey results and compatibility logic
    app.post("/api/friends", function(req, res){
+//Get data and add new friend to friends array
       var newFriend = req.body;
-      console.log(newFriend);
       friends.push(newFriend);
-      res.json(newFriend);
+
+      var newFriendScore = newFriend.score;
+//Loop through existing friend's score  
+      for(var i = 0; i < friends.length; i++){
+         var friendScore = friends[i].score;
+//Loop through new friend's score     
+      for(var x = 0; x < newFriendScore.length; x++){
+         if(friendScore == newFriendScore){
+            $("You're a perfect match with " + res.json(friends[i]) + "!");
+         };
+      };
+      };
    });
+
+
+
+   
+
 };
